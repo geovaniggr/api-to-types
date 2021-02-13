01893f9c848bcc09e9f601b0571f079617929ea5
geovaniggr <geovani.hilario@outlook.com>
2021-02-13 15:13:50 -0300

    feat: add mock for local.js

---
+++ mock.js
@@ -0,0 +1,15 @@
+export default {
+    nome: "Geovani",
+    idade: 21,
+    premium: true,
+    tecnologias: [
+        {nome: "Clojure", tipo: "Funcional", experiencia: 8},
+        {nome: "Elixir", tipo: "Funcional", experiencia: 0},
+        {nome: "Java", tipo: "OO", experiencia: 8}
+    ],
+    localizacao: {
+        nome: "Brasil",
+        estado: "São Paulo",
+        cidade: "São Paulo"
+    }
+}
